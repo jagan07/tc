6a42153d295fc8a64fda92b0733f5cdece1adba9
jagan07 <47105628+jagan07@users.noreply.github.com>
2019-02-11 15:52:26 +0530

    Create task1.c

---
+++ task1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+scanf("%d,&n);
+if(n>0)
+{
+printf("positive");
+}
+elseif(n=0)
+{
+printf("zero");
+}
+elseif(n<0)
+{
+printf("negative");
+}
+getch();
+}
